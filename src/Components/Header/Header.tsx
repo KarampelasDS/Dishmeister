4003881d97b591b339a13879011dfd1d8612c260
Dimitrios-Spiridon Karampelas <24dimitris@gmail.com>
2026-02-14 20:42:46 +0200

    Made test burger menu css breakpoint

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,18 +1,22 @@
 import styles from "./Header.module.css";
 import AppLogo from "../AppLogo/AppLogo";
 import UserMenu from "./UserMenu/UserMenu";
+import BurgerMenu from "../BurgerMenu/BurgerMenu";
 
 export default function Header(props: any) {
   return (
     <div className={styles.HeaderContainer}>
-      <div>
+      <div className={styles.appLogoContainerDesktop}>
         <AppLogo />
       </div>
-      <div>
+      <div className={styles.searchBarContainerDesktop}>
         <h1>searchbar</h1>
       </div>
-      <div>
+      <div className={styles.userMenuContainerDesktop}>
         <UserMenu username={props.username} avatarUrl={props.avatarUrl} />
+      </div>
+      <div className={styles.burgerMenu}>
+        <BurgerMenu />
       </div>
     </div>
   );
