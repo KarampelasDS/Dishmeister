4752af1c15753b2a48e6fc98b7c8d642d707bc86
Dimitrios-Spiridon Karampelas <24dimitris@gmail.com>
2026-03-28 23:56:44 +0200

    Minor fix

--- src/Components/RecipeView/RecipeView.tsx
+++ src/Components/RecipeView/RecipeView.tsx
@@ -448,7 +448,6 @@ export default function RecipeView({
             </div>
           </section>
 
-          {/* INSTRUCTIONS */}
           <section>
             <div className={styles.sectionTitle}>
               <UtensilsCrossed color="#f97316" size={26} />
